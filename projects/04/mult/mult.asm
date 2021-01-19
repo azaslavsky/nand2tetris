d60963277b4c98785ae086a031d73176650e6f77
azaslavsky <alexz@speedshare.com>
2021-01-18 17:31:12 -0800

    Finished 04 (mult)

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,38 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+// Set R2 to 0.
+@R2
+AM=0
+
+// Adds R0 to R1, as long as R1 > 0.
+(LOOP)
+  // Set D to the value stored in R1.
+  @R1
+  D=M
+
+  // If R1 == 0, terminate the program.
+  @END
+  D;JEQ
+
+  // Decrement R1.
+  @R1
+  AM=D-1
+
+  // Set D to the value stored in R0.
+  @R0
+  D=M
+
+  // Adds the value in D (ie, the value from R0) to the value in A (the current R2).
+  @R2
+  AM=M+D
+  
+  // Restart LOOP.
+  @LOOP
+  0;JMP
+
+// End state - if this is reached, R2 is populated correctly and program exits.
+(END)
+  @END
+  0;JMP
